f22d9ba6f23bd6dbd498ab85433f7a7c88e2d4d9
Ryan <ryanyun2010@gmail.com>
2025-04-03 05:40:55 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
-const DAMPING: f64 = 0.85;
+#![allow(clippy::all)]
 
-
+use core::f64;
 pub struct Web {
-    pub sites: Vec<Site>,
-    pub ranks: Vec<f64>
-}
-
-pub struct Site {
-    pub links: Vec<usize>
+    pub s: Vec<Vec<usize>>,
+    pub r: Vec<f64>
 }
 
 impl Web {
-    pub fn rank(&mut self, iterations: usize) -> &Vec<f64>{
-        // ive tried to do this all in one line without using a semi-colon, so in a way this is 0 lines 
-            (0..iterations).map(|_| (0..self.sites.len()).map(|site| self.ranks[site] = (1.0 - DAMPING) + DAMPING * self.sites.iter().enumerate().map(|(i, x)| if x.links.contains(&site) {self.ranks[i]/x.links.len() as f64}else{0.0}).sum::<f64>()).last()).last().map(|_| &self.ranks).unwrap()
+    pub fn rank(self, i: usize, d: f64) -> Vec<f64>{
+        // page rank calculation in one line, 143 characters
+(0..i).fold(self.r,|z,_|(0..z.len()).map(|q|self.s.iter().zip(&z).fold(1.-d,|c,(s,r)|if s.contains(&q){c+r*d/s.len()as f64}else{c})).collect())
+    }
+    pub fn rank_explained(self, i: usize, d: f64) -> Vec<f64> {
+
+(0..i)  // we use 0..i while ignoring the actual value to do it the number of iterations
+    .fold(self.r, |z, _| // Fold over the range (0..i) starting with the initial value self.r (the rank vec) storing the current rank vec in z
+        (0..z.len()).map(|q| // iterate through each site id, storing the current id in q
+            self.s.iter().zip(&z) // iterate through all the sites and their ranks
+                // Fold over the zipped iterator, starting with an initial value of (1 - d)
+                .fold(1. - d, |c, (s, r)| 
+                    if s.contains(&q) { // if the site contains the current site id in its links
+                        c + r * d / s.len() as f64 // add its pagerank / links * d
+                    } else {
+                        c // otherwise dont add anything 
+                    }
+                )
+        ).collect() // collect the results of this mapping into the new vec of z 
+    )
+
     }
 }
 
 
-
-
-
 fn main() {
-    let mut web = Web {
-        sites: vec![Site {
-            links: vec![1,2]
-        }, Site{
-            links: vec![2]
-        }, Site{
-            links: vec![0]
-        }, Site{
-            links: vec![2]
-        }],
-        ranks: vec![0.25, 0.25, 0.25, 0.25]
+    let web = Web {
+        s: vec![vec![1,2], vec![2], vec![0], vec![2]],
+        r: vec![0.25, 0.25, 0.25, 0.25]
     };
-    println!("{:?}", web.rank(35));
+        let time = std::time::Instant::now();
+    println!("{:?}", web.rank(35, 0.85));
+    println!("time {:?}", time.elapsed());
 }
